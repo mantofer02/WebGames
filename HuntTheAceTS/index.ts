5cd6d8cd754ea131cc066902246c180c7bfb166d
Marco Ferraro <mantofer2000@gmail.com>
2023-06-10 17:47:40 -0600

    adding function to render and create components dynamically

--- HuntTheAceTS/index.ts
+++ HuntTheAceTS/index.ts
@@ -1,19 +1,9 @@
-const cardObjectDefinition = [
-  { id: 1, imagePath: '/assets/images/king_of_hearts2.svg' },
-  { id: 2, imagePath: '/assets/images/jack_of_clubs2.svg' },
-  { id: 3, imagePath: 'assets/images/queen_of_diamonds2.svg' },
-  { id: 4, imagePath: 'assets/images/ace_of_spades2.svg' },
-];
-
-function createCart(cardItem) {
-  const cardElem: HTMLElement = createElement('div');
-  const cardInnerElem: HTMLElement = document.createElement('div');
-  const cardFrontElem: HTMLElement = document.createElement('div');
-  const cardBackElem: HTMLElement = document.createElement('div');
-
-  const cardFrontImg: HTMLElement = createElement('img');
-  const cardBackImg: HTMLElement = createElement('img');
-}
+import {
+  CardObject,
+  cardObjects,
+  cardBackImgPath,
+  cardConainerElem,
+} from '@cardType';
 
 function createElement(elemType: string): HTMLElement {
   return document.createElement(elemType);
@@ -21,4 +11,46 @@ function createElement(elemType: string): HTMLElement {
 
 function addClassToElement(elem: HTMLElement, className: string) {
   elem.classList.add(className);
+}
+
+function addIdToElement(elem: HTMLElement, id: number) {
+  elem.id = id.toString();
+}
+
+function addSrcToImageElem(imgElemen: HTMLImageElement, src: string) {
+  imgElemen.src = src;
+}
+
+function addChildElement(parentElem: HTMLElement, childElem: HTMLElement) {
+  parentElem.appendChild(childElem);
+}
+
+function createCart(cardItem: CardObject) {
+  const cardElem: HTMLElement = createElement('div');
+  const cardInnerElem: HTMLElement = document.createElement('div');
+  const cardFrontElem: HTMLElement = document.createElement('div');
+  const cardBackElem: HTMLElement = document.createElement('div');
+
+  const cardFrontImg: HTMLElement = createElement('img');
+  const cardBackImg: HTMLElement = createElement('img');
+
+  addClassToElement(cardElem, 'card');
+  addIdToElement(cardElem, cardItem.id);
+
+  addClassToElement(cardInnerElem, 'card__inner');
+  addClassToElement(cardFrontElem, 'card__front');
+  addClassToElement(cardBackElem, 'card__back');
+
+  addClassToElement(cardFrontImg, 'card__img');
+  addClassToElement(cardBackImg, 'card__img');
+
+  addSrcToImageElem(cardFrontImg as HTMLImageElement, cardItem.imagePath);
+  addSrcToImageElem(cardBackImg as HTMLImageElement, cardBackImgPath);
+
+  addChildElement(cardFrontElem, cardFrontImg);
+  addChildElement(cardBackElem, cardBackImg);
+
+  addChildElement(cardInnerElem, cardFrontElem);
+  addChildElement(cardInnerElem, cardBackElem);
+  addChildElement(cardElem, cardInnerElem);
 }
